755222b15fc9b8841e1dd050a395a375439874a7
Kavindu Dassanayake <kavindud@AFSL.local>
2026-06-13 10:39:13 +0530

    DB password change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongourl : 'mongodb+srv://kavindugaya2002_db_user:<db_password>@vidjot-db.wnpyxzt.mongodb.net/?appName=Vidjot-DB'}
+    module.exports = {mongourl : 'mongodb+srv://kavindugaya2002_db_user:Das2002@vidjot-db.wnpyxzt.mongodb.net/?appName=Vidjot-DB'}
 }else{
     module.exports = {mongourl : 'mongodb://localhost/vidjot_DB'}
 }
